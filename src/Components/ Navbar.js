3812e4e256127f0fd3a2bdab52574b9e6b004515
Nixon Pastor <nixonpastorguzman@gmail.com>
2021-02-20 20:03:30 -0700

    Added some navbar styling

--- src/Components/ Navbar.js
+++ src/Components/ Navbar.js
@@ -1,31 +1,40 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/"> Home</Link>
-      <div>
-        <ul>
+    <nav className="navbar">
+      <ul className="LinkBox">
+        <div className="RattlerLogo">
           <li>
-            <Link to="/electronics">Electronics</Link>
+            <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/dormdecor">Dorm Decor</Link>
-          </li>
-          <li>
-            <Link to="/books">Books</Link>
-          </li>
-          <li>
-            <Link to="/apparel">Apparel</Link>
-          </li>
-          <li>
-            <Link to="/wishlist">Wishlist</Link>
-          </li>
-          <li>
-            <Link to="/profile">My Profile</Link>
-          </li>
-        </ul>
-      </div>
+        </div>
+        <div className="NavbarRightContainer">
+          <div className="CategoryLinks">
+            <li>
+              <Link to="/electronics">Electronics</Link>
+            </li>
+            <li>
+              <Link to="/dormdecor">Dorm Decor</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/apparel">Apparel</Link>
+            </li>
+          </div>
+          <div className="NavIcons">
+            <li>
+              <Link to="/wishlist">Wishlist</Link>
+            </li>
+            <li>
+              <Link to="/profile">My Profile</Link>
+            </li>
+          </div>
+        </div>
+      </ul>
     </nav>
   );
 }
