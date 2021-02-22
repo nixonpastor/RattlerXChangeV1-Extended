e0881dcb571d8d1fafc65c00f983f570f07a0a0e
Nixon Pastor <nixonpastorguzman@gmail.com>
2021-02-21 19:28:15 -0700

    Built the cards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 import CardRender from "./CardRender";
+import "./Card.css";
 function Home() {
   return (
     <div>
       <h1>Welcome to Home Page</h1>
-      <CardRender />
+      <ul className="CardsContainer">
+        <CardRender />
+        <CardRender />
+        <CardRender />
+        <CardRender />
+        <CardRender />
+        <CardRender />
+        <CardRender />
+      </ul>
     </div>
   );
 }
