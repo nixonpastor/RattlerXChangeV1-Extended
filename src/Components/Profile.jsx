c7043846fe992f33586ed4859817c782f1d7d70d
jzordilla <jmzordilla@gmail.com>
2021-02-27 14:26:28 -0600

    fleshed out profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,41 @@
-function Profile() {
-  return <h1>Welcome to Profile Page</h1>;
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
+import "./Profile.css"
+
+function Profile(props) {
+  return (
+    <div className = "userInfo">
+      <h1>Welcome to Profile Page</h1>
+      <div className = "ProfileTag">My Profile</div>
+      <img className = "image" alt = "profile pic" src="logo192.png" text={props.text} value={props.value} />
+      
+      {/* Would like to link to file browser */}
+      <li className = "profilePic">Change Picture</li>
+      
+      <div className = "Name">Full Name</div>
+      
+      {/* Used link here just in case we want to have this take
+          the person straight to Outlook */}
+      <li className = "Email">Email</li>
+      <div className = "productsListed">No. of Products Listed</div>
+      <li className = "link">
+          <Link to = "/editProfile" className = "links">
+            Edit Profile
+          </Link>
+      </li>
+      <li className = "link">
+          <Link to = "/addProduct" className = "links">
+            Add a Product
+          </Link>
+      </li>
+      <li className = "link">
+          <Link to = "/signIn" className = "links">
+            Log Out
+          </Link>
+      </li>
+      <Footer/>
+    </div>
+  );
 }
 
 export default Profile;
