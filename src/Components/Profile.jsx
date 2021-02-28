2c53463ea72c3a91b4468600d41a21286963310e
Nixon Pastor <nixonpastorguzman@gmail.com>
2021-02-27 17:55:46 -0700

    Fixed footer on pages

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,45 +1,50 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
-import "./Profile.css"
+import "./Profile.css";
+import "./Pages.css";
 
 function Profile(props) {
   return (
-    <div className = "ProfilePage">
-      <h1 className = "title">Welcome to Profile Page</h1>
-      <div className = "userInfo">
-        <div className = "ProfileTag">
-          <b className = "tag">
-            My Profile
-          </b>
+    <div className="pageContent">
+      <h1 className="title">Welcome to Profile Page</h1>
+      <div className="userInfo">
+        <div className="ProfileTag">
+          <b className="tag">My Profile</b>
         </div>
-        <img className = "image" alt = "profile pic" src="logo192.png" text={props.text} value={props.value} />
-      
+        <img
+          className="image"
+          alt="profile pic"
+          src="logo192.png"
+          text={props.text}
+          value={props.value}
+        />
+
         {/* Would like to link to file browser */}
-        <li className = "profilePic">Change Picture</li>
-      
-        <div className = "Name">Full Name</div>
-      
+        <li className="profilePic">Change Picture</li>
+
+        <div className="Name">Full Name</div>
+
         {/* Used link here just in case we want to have this take
           the person straight to Outlook */}
-        <li className = "Email">Email</li>
-        <div className = "productsListed">No. of Products Listed</div>
+        <li className="Email">Email</li>
+        <div className="productsListed">No. of Products Listed</div>
       </div>
-      <li className = "link">
-          <Link to = "/editProfile" className = "links">
-            Edit Profile
-          </Link>
+      <li className="link">
+        <Link to="/editProfile" className="links">
+          Edit Profile
+        </Link>
       </li>
-      <li className = "link">
-          <Link to = "/addProduct" className = "links">
-            Add a Product
-          </Link>
+      <li className="link">
+        <Link to="/addProduct" className="links">
+          Add a Product
+        </Link>
       </li>
-      <li className = "link">
-          <Link to = "/signIn" className = "links">
-            Log Out
-          </Link>
+      <li className="link">
+        <Link to="/signIn" className="links">
+          Log Out
+        </Link>
       </li>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
