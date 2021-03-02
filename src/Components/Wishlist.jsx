06593ce3d7486ada88c46859e57b79a3601a32cb
Pragyan KC <pragyankc@Pragyans-MacBook-Pro.local>
2021-03-01 22:58:59 -0600

    Wishlist Added

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -1,5 +1,24 @@
+import CardRender from "./CardRender";
+import SearchAndSortRender from "./SearchAndSortRender";
+import Footer from "./Footer";
+import "./Pages.css";
+import navbar from "./Navbar.css";
+
 function Wishlist() {
-  return <h1>Welcome to Wishlist Page</h1>;
+  return (
+    <div className="pageContent">
+      <navbar />
+      <SearchAndSortRender Title="Wishlist" />
+      <ul className="CardsContainer"> {/* select favorites */}
+        <CardRender text="St. Mary's Shirt" value="$80" />
+        <CardRender text="iPhone 12" value="$800" />
+        <CardRender text="Desk Lamp" value="$100" />
+        <CardRender text="Curtains" value="$30" />
+        <CardRender text="Calculus I" value="$150" />
+      </ul>
+      <Footer />
+    </div>
+  )
 }
 
 export default Wishlist;
