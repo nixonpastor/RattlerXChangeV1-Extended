6004aa084ae4a986e607a41473da6439d1a07cb1
nguduru007 <76142769+nguduru007@users.noreply.github.com>
2021-03-06 16:13:04 -0600

    Update EditProduct.jsx

--- src/Components/EditProduct.jsx
+++ src/Components/EditProduct.jsx
@@ -37,36 +37,7 @@ function EditProduct() {
                     </form>
                 </div>
             </div>
-<<<<<<< HEAD
-            <div className = "productLabels">
-            <form>
-                <label>
-                    Product Name: 
-                    <input type="text" name="Edit Product name" placeholder = "Edit Product name" />
-                </label>
-                <label>
-                    Cost: 
-                    <input 
-                    type="text" name="Edit Cost of the Product" placeholder = "Edit Cost of the Product" />
-                </label>
-                <label>
-                    Product Condition : 
-                    <input type="text" name="Edit Product Condition" placeholder = "Edit Product Condition" />
-                </label>
-                <label>
-                    Category: 
-                    <input type="text" name="Select Category" placeholder = "Select Category" />
-                </label>
-                <label>
-                    Description: 
-                    <input type="text" name="Product Description" placeholder = "Edit Description" />
-                </label>
-                <input type="submit" value="Save Changes" id="submitButton" />
-            </form>
-            </div>
-=======
             <Footer />
->>>>>>> 91c08fbb4c506926e6cc5c8abab642cd26d624d7
         </div>
     );
 }
