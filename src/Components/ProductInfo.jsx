e1c6e6239afc90a2d0ce5b99f6e0811ea729b870
Nixon Pastor <nixonpastorguzman@gmail.com>
2021-03-28 18:31:38 -0700

    Added Home Products and Product Info

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -1,63 +1,141 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import "./ProductInfo.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function ProductInfo() {
-    return (
-        <div className="pageContent">
-            <h2 className="productInfoHeader">Product Information</h2>
-            <div className="mainProductInfo">
-                <div className="imageProductInfo">
-                    <button className="productInfoImageButton" /*style="background: url('Rattler.jpeg')"*/> Image Goes Here
-                </button>
-                </div>
-                <div className="productLabels">
-                    <form className="productInfoForm">
-                        <label>
-                            Product Name:
-                    <input type="button" name="Product Name" className="productInfoField" value="Product name not available" />
-                        </label>
-                        <label>
-                            Cost:
-                    <input type="button" name="Cost" className="productInfoField" value="Cost not available" />
-                        </label>
-                        <label>
-                            Product Condition :
-                    <input type="button" name="Condition" className="productInfoField" value="Condition not available" />
-                        </label>
-                        <label>
-                            Category:
-                    <input type="button" name="Category" className="productInfoField" value="Category not available" />
-                        </label>
-                        <label>
-                            Description:
-                    <input type="button" name="Description" className="productInfoField" value="Description not avaiable" />
-                        </label>
+function ProductInfo(props) {
+  //   console.log(props.location.productProps.productId);
+  //   //   const [product, setProduct] = useState(null);
+  //   const [isLoading, setLoading] = useState(true);
 
-                        <div className="buttons">
-                            <div>
-                                <button type="submit" className="sellerInfoButton">
-                                    Contact Seller
-                            <div class="buttonTextSpace" />
-                                    <i class="fas fa-envelope"></i>
-                                </button>
-                            </div>
-                            <div class="divider" />
-                            <div>
-                                <button type="submit" className="wishlistButton">
-                                    Wishlist
-                            <div class="buttonTextSpace" />
-                                    <i class="fas fa-heart"></i>
-                                </button>
-                            </div>
-                        </div>
+  //   axios
+  //     .get(
+  //       "http://localhost:5000/products/" + props.location.productProps.productId
+  //     )
+  //     .then((res) => {
+  //       if (isLoading) {
+  //         console.log(res.data);
+  //         // setProduct(res.data);
+  //         // console.log(product);
+  //         setLoading(false);
+  //       }
+  //     });
 
-                    </form>
-                </div>
-            </div>
-            <Footer />
+  const [product, setProduct] = useState({
+    _id: props.location.productProps.productId,
+    productName: "No Product Info",
+    productPrice: 0,
+    productCondition: "No Condition Found",
+    productCategory: "No Category Found",
+    productDescription: "Null",
+    __v: 0,
+  });
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    function getProducts() {
+      if (props.location.productProps.productId !== null) {
+        axios
+          .get(
+            "http://localhost:5000/products/" +
+              props.location.productProps.productId
+          )
+          .then((res) => {
+            if (isLoading) {
+              setProduct(res.data);
+              console.log(product);
+              setLoading(false);
+            }
+          });
+      }
+    }
+    getProducts();
+  }, [product, isLoading, props.location.productProps.productId]);
+
+  return (
+    <div className="pageContent">
+      <h2 className="productInfoHeader">Product Information</h2>
+      <div className="mainProductInfo">
+        <div className="imageProductInfo">
+          <button
+            className="productInfoImageButton" /*style="background: url('Rattler.jpeg')"*/
+          >
+            {" "}
+            Image Goes Here
+          </button>
         </div>
-    );
+        <div className="productLabels">
+          <form className="productInfoForm">
+            <label>
+              Product Name:
+              <input
+                type="button"
+                name="Product Name"
+                className="productInfoField"
+                value={product.productName}
+              />
+            </label>
+            <label>
+              Cost:
+              <input
+                type="button"
+                name="Cost"
+                className="productInfoField"
+                value={product.productPrice}
+              />
+            </label>
+            <label>
+              Product Condition :
+              <input
+                type="button"
+                name="Condition"
+                className="productInfoField"
+                value={product.productCondition}
+              />
+            </label>
+            <label>
+              Category:
+              <input
+                type="button"
+                name="Category"
+                className="productInfoField"
+                value={product.productCategory}
+              />
+            </label>
+            <label>
+              Description:
+              <input
+                type="button"
+                name="Description"
+                className="productInfoField"
+                value={product.productDescription}
+              />
+            </label>
+
+            <div className="buttons">
+              <div>
+                <button type="submit" className="sellerInfoButton">
+                  Contact Seller
+                  <div class="buttonTextSpace" />
+                  <i class="fas fa-envelope"></i>
+                </button>
+              </div>
+              <div class="divider" />
+              <div>
+                <button type="submit" className="wishlistButton">
+                  Wishlist
+                  <div class="buttonTextSpace" />
+                  <i class="fas fa-heart"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default ProductInfo;
